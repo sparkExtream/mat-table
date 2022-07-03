652066bf0b6fa80d96167fd8ccf022206c9f7156
@himal-qsstechnosoft <himalbhandari@himals-MacBook-Air.local>
2022-07-03 23:11:46 +0530

    updated

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-//Install express server
-const express = require("express");
-const path = require("path");
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static("./dist/companies-table"));
+app.use(requireHTTPS);
+app.use(express.static('./dist/assignment-app'));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/companies-table/" })
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/assignment-app/'}),
 );
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
